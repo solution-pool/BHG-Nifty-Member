e76373f28ce7ffd1fe6fa9433120a896b34f3b47
J-Gambler <antonovserg943@gmail.com>
2021-11-17 05:57:53 +0300

    style fix on the footer bar

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -16,19 +16,19 @@ const Profile = () => {
                 <Form>
                     <Row>
                         <Col lg="4" md="6" sm="12" className="main-col">
-                            <Form.Group className="mb-4" controlId="formUsername">
+                            <Form.Group controlId="formUsername">
                                 <Form.Label>Create Username</Form.Label>
                                 <Form.Control type="text" placeholder="Use Wallet Address(default)" />
                             </Form.Group>
                         </Col>
                         <Col lg="4" md="6" sm="12" className="main-col">
-                            <Form.Group className="mb-4" controlId="formPassword">
+                            <Form.Group controlId="formPassword">
                                 <Form.Label>Create Password</Form.Label>
                                 <Form.Control type="text" placeholder="Create Password" />
                             </Form.Group>
                         </Col>
                         <Col lg="4" md="6" sm="12" className="main-col">
-                            <Form.Group className="mb-4" controlId="formStatus">
+                            <Form.Group controlId="formStatus">
                                 <Form.Label>Profile Status</Form.Label>
                                 <Form.Control type="text" placeholder="" />
                             </Form.Group>
@@ -40,93 +40,93 @@ const Profile = () => {
                             </Form.Group>
                         </Col>
                         <Col lg="4" md="6" sm="12" className="main-col">
-                            <Form.Group className="mb-4" controlId="formRole">
+                            <Form.Group controlId="formRole">
                                 <Form.Label>Role</Form.Label>
                                 <Form.Control type="text" placeholder="Role" />
                             </Form.Group>
                         </Col>
                         <Col lg="4" md="6" sm="12" className="main-col">
-                            <Form.Group className="mb-4" controlId="formHeld">
+                            <Form.Group controlId="formHeld">
                                 <Form.Label>Nodestone(s) Held</Form.Label>
                                 <Form.Control type="text" placeholder="Nodestone(s) Held" />
                             </Form.Group>
                         </Col>
                         <Col lg="12" md="12" className="main-col">
-                            <Form.Group className="mb-4" controlId="formHeld">
+                            <Form.Group controlId="formHeld">
                                 <Form.Label>Nodestone(s) Held</Form.Label>
-                                <Container>
+                                <Container className="checkbox-panel">
                                     <Row>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Artist" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Musician" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Web Dev" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Project Manager" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Business Dev" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Game Dev" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Marketing/Promotion" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Data Analyst" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Collector" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Smart Contracts" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="AI" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Social Media Manager" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Photographer" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Discord Mod" />
                                             </Form.Group>
                                         </Col>
-                                        <Col lg="4" md="6" sm="12">
+                                        <Col lg="4" md="6" sm="12" className="checkbox-col">
                                             <Form.Group className="mb-3">
                                                 <Form.Check type="checkbox" label="Other" />
                                             </Form.Group>
@@ -138,22 +138,22 @@ const Profile = () => {
                         <Col lg="4" className="main-col">
                             <Form.Group className="mb-4">
                                 <Form.Label>Bio</Form.Label>
-                                <Form.Control as="textarea" rows={3} placeholder="Bio" />
+                                <Form.Control className="footer-element" as="textarea" rows={3} placeholder="Bio" />
                             </Form.Group>
                         </Col>
                         <Col lg="4" className="main-col">
                             <Form.Group className="mb-4">
                                 <Form.Label style={{visibility:'hidden'}}>Bio</Form.Label>
-                                <div>
-                                    <Button className="light">Choose File(s)</Button>
+                                <div className="footer-element file-panel">
+                                    <Button variant="light">Choose File(s)</Button>
                                 </div>
                             </Form.Group>
                         </Col>
                         <Col lg="4" className="main-col">
                             <Form.Group className="mb-4">
                                 <Form.Label style={{visibility:'hidden'}}>Bio</Form.Label>
-                                <div>
-                                    <Button className="light">Submit</Button>
+                                <div className="footer-element submit-panel">
+                                    <Button variant="secondary">Submit</Button>
                                 </div>
                             </Form.Group>
                         </Col>
