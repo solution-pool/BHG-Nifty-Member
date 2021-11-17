9b594f978caeda2a1044532e8189271a39485fef
J-Gambler <antonovserg943@gmail.com>
2021-11-17 16:25:08 +0300

    front end style complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,23 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="#home">Nifty</Navbar.Brand>
+                <Navbar.Brand href="#">Nifty</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">
-
+                        <Nav.Link href="https://twitter.com">
+                            <i className="fab fa-twitter"></i>
                         </Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="https://discord.com">
+                            <i className="fab fa-discord"></i>
+                        </Nav.Link>
+                        <Nav.Link href="https://opensea.io">
+                            <img src={require('../assets/img/logo.svg').default} />
+                        </Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link>
-                            <Button variant="white">Connect Wallet</Button>
+                            <Button variant="light" id="connect-wallet">Connect Wallet</Button>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
