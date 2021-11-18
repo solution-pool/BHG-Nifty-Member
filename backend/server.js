f13e37b23b5104b6ca4fb81a1cea689b7e5482ce
J-Gambler <antonovserg943@gmail.com>
2021-11-18 20:53:07 +0300

    upload file and save data

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,54 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
+// import express (after npm install express)
+const express = require('express');
+const fs = require('fs');
+const multer = require('multer');
+const cors = require('cors');
 const mysql = require('mysql')
 const db = require('./config/db')
-const fs = require('fs')
 
-const app = express()
+// create new express app and save it as "app"
+const app = express();
 
-const port = 8000
+// server configuration
+const PORT = 8000;
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+app.use(cors());
 
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'resources')
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + "-" + file.originalname);
+	}
+})
 
-
-app.post('/', (req, res) => {
+const upload = multer({ storage: storage }).single('file');
+// create a route for the app
+app.post('/',function(req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+         	return res.status(500).json(err)
+		}
+    })
+    
     const con = mysql.createConnection(db)
     con.connect(function (err) {
         if(err)  {
             throw err;
         }
-        console.log('connected')
 
-        const file = req.body.file
-        console.log(req.file)
-
-        var sql = `INSERT INTO nifty (username, password, status, email, role, held, interest, bio, file, sha256) VALUES ("${req.body.username}", "${req.body.password}", "${req.body.status}", "${req.body.email}", "${req.body.role}", "${req.body.held}", "${req.body.interest}", "${req.body.bio}", "${req.body.file}", "${req.body.sha256}")`;
+        var sql = `INSERT INTO nifty (username, password, status, email, role, held, interest, bio, file, sha256) VALUES ("${req.body.username}", "${req.body.password}", "${req.body.status}", "${req.body.email}", "${req.body.role}", "${req.body.held}", '${req.body.interest}', "${req.body.bio}", "${req.body.fileName}", "${req.body.fileName}")`;
         con.query(sql, function (err, result) {
             if(err) throw err;
             console.log("Result: " + result)
         })
     })
-    res.send('Hello')
-})
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-    console.log('We are live on ' + port)
-})
+	return res.status(200).send({ data: 'success' });
+});
+
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
