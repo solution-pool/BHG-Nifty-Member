f359c504fa6eb4c557adfb331dbdb565c32a7e5a
J-Gambler <antonovserg943@gmail.com>
2021-11-20 21:27:19 +0300

    modify external site links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,15 +9,15 @@ const Header = (props) => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="https://twitter.com">
+                        <Nav.Link href="https://twitter.com/nifty_io">
                             <span>Twitter</span>
                             <i className="fab fa-twitter"></i>
                         </Nav.Link>
-                        <Nav.Link href="https://discord.com">
+                        <Nav.Link href="https://discord.com/nifty">
                             <span>Discord</span>
                             <i className="fab fa-discord"></i>
                         </Nav.Link>
-                        <Nav.Link href="https://opensea.io">
+                        <Nav.Link href="https://opensea.io/collection/node_stones">
                             <span>Opensea</span>
                             <img src={require('../assets/img/logo.svg').default} alt="Opensea icon" />
                         </Nav.Link>
